2c3e5e57c5f5e881426c622420effeacb7dddbf8
Muhammad <gitbytalha@yahoo.com>
2022-11-19 01:36:28 +0500

    initialize commit

---
+++ stress_02.py
@@ -0,0 +1,13 @@
+#Nuclear physics is the brutal branch of the physics
+#as well as the Science
+#But we know very well that
+print("The whole galaxies are move around the application of physics")
+#Physicist125, snapchat id
+#go and follow me through that platform xd:)
+print("Recently we are overcome from the lost of Worldcup 2022 men's")
+#win and loss is the part of game
+#kindly avoid to regret about that
+print("So we have to learn from our mistakes")
+#.....
+#......
+#.......abs(Behtareen Boss)
