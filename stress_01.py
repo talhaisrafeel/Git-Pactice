e6afb6aea862d2096133d1c5800b815ead52157f
Muhammad <gitbytalha@yahoo.com>
2022-11-19 00:53:13 +0500

    Ad my files fast lol

---
+++ stress_01.py
@@ -0,0 +1,6 @@
+#Some people have curly brown hair
+#you guys know that's quite lame joke in trignometry
+#but you have to bear that
+print('''Really maths is the shit subject for many students,
+ specify one of my students too xd:) ''')
+ 
