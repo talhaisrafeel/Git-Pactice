3ff26dd7ca96cf02fa7ea86694950267d88c9086
Muhammad Talha <116680508+talhaisrafeel@users.noreply.github.com>
2022-11-20 00:43:45 +0500

    Add files via upload

---
+++ stress_03.py
@@ -0,0 +1,17 @@
+#Now, I think that i have to come at Loops
+#what's the purpose of loops?
+#let me explain stupid.
+a = '''Muhammad Talha Israfeel'''
+b = 625
+c = 29.065
+d = False
+#now I'm upto doing practice for variables
+print(a)
+print(b)
+print(c)
+print(d)
+#now we et to known class of the variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d)) 
